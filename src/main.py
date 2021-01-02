455025b754eb99e9ba9f23a4ad7e8a681e1d2515
omorest <oscar.moreira.estevez@gmail.com>
2021-01-02 12:30:26 +0000

    main

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import entrenando, reconocimientoEmociones
+
+METHOD = ''
+
+def menu():
+  print("Elija el método de entrenamiento")
+  print("1 -> EigenFaces")
+  print("2 -> LBPH")
+  print("3 -> FisherFaces")
+
+  option_method = int(input())
+  if option_method == 1:
+      METHOD = "EigenFaces"
+  if option_method == 2:
+      METHOD = "LBPH"
+  if option_method == 3:
+      METHOD = "FisherFaces"
+  
+  entrenando.obtenerModelo(METHOD, entrenando.facesData, entrenando.labels)
+
+  reconocimientoEmociones.reconocimiento(METHOD)
+
+
+
+menu()

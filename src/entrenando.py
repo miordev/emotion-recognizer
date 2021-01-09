c4fd003f57973dd864c56f35dfc388a5c02e17bc
Miguel Angel <j2mc1410@gmail.com>
2021-01-09 16:46:12 +0000

    Comentado entrenamiento

--- src/entrenando.py
+++ src/entrenando.py
@@ -3,40 +3,59 @@ import os
 import numpy as np
 import time
 
-def obtenerModelo(method,facesData,labels):    
-  if method == 'EigenFaces': emotion_recognizer = cv2.face.EigenFaceRecognizer_create()
-  if method == 'FisherFaces': emotion_recognizer = cv2.face.FisherFaceRecognizer_create()
-  if method == 'LBPH': emotion_recognizer = cv2.face.LBPHFaceRecognizer_create()
+import methods
+import emotions
+import paths
 
-  # Entrenando el reconocedor de rostros
-  print("Entrenando ( "+method+" )...")
-  inicio = time.time()
-  emotion_recognizer.train(facesData, np.array(labels))
-  tiempoEntrenamiento = time.time()-inicio
-  print("Tiempo de entrenamiento ( "+method+" ): ", tiempoEntrenamiento)
 
-  # Almacenando el modelo obtenido
-  if not os.path.exists('../models'):
-    os.mkdir('../models')
+def train_emotion_recognizer(method_name, model, faces_data, labels):
+  print('Entrenando ( ' + method_name + ' )...')
+  
+  initial_time = time.time()
+  model.train(faces_data, np.array(labels))
+  final_time = time.time()
+
+  train_time = final_time - initial_time
+  print('Tiempo de entrenamiento: ', train_time)
+
+
+def save_emotion_recognizer(method_name, model):
+  if not os.path.exists(paths.MODELS_PATH):
+    os.mkdir(paths.MODELS_PATH)
     
-  emotion_recognizer.write("../models/modelo"+method+".xml")
+  specific_model_path = paths.MODELS_PATH + '/modelo' + method_name + '.xml'
+  model.write(specific_model_path)
 
 
-dataPath = '../data' #Cambia a la ruta donde hayas almacenado Data
-emotionsList = os.listdir(dataPath)
-print('Lista de personas: ', emotionsList)
+def create_model(method_name, faces_data, labels):
+  # Dependiendo del nombre del método, crea un modelo
+  model = methods.get_emotion_recognizer(method_name)
 
+  # Hace el entranamiento con el conjunto de imágenes (escala de grises) y etiquetas
+  train_emotion_recognizer(method_name, model, faces_data, labels)
+
+  # Guarda el modelo en un archivo 'xml'
+  save_emotion_recognizer(method_name, model)
+
+
+def define_data(faces_data, labels):
+  label = 0
+  for nameDir in emotions.ALL:
+    emotions_path = paths.DATA_PATH + '/' + nameDir
+
+    for filename in os.listdir(emotions_path):
+      labels.append(label)
+
+      face_path = emotions_path + '/' + filename      
+      image = cv2.imread(face_path, cv2.IMREAD_GRAYSCALE)
+      faces_data.append(image)
+
+    label += 1
+
+
+# Conjunto de caras y etiquetas, clasificados según su emoción
 labels = []
-facesData = []
-label = 0
+faces_data = []
 
-for nameDir in emotionsList:
-  emotionsPath = dataPath + '/' + nameDir
-
-  for fileName in os.listdir(emotionsPath):
-    labels.append(label)
-    facesData.append(cv2.imread(emotionsPath+'/'+fileName,0))
-    image = cv2.imread(emotionsPath+'/'+fileName,0)
-    # cv2.imshow('image',image)
-    cv2.waitKey(10)
-  label = label + 1
+define_data(faces_data, labels)
+create_model(methods.LBPH, faces_data, labels)

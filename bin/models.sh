575e610d1eb640cba213b7e34f66e853ccc0921a
adal <alu0101112251@ull.edu.es>
2021-01-22 17:00:15 +0000

    ADD_ Image browser

--- bin/models.sh
+++ bin/models.sh
@@ -2,23 +2,23 @@ dirSaveFiles=models
 rm -r ${dirSaveFiles}
 mkdir ${dirSaveFiles}
 
-# FICHER -> https://drive.google.com/file/d/1neSH23XUAvqdSDHNDq5T47mPoLmsz8yM/view?usp=sharing
-fileId=1neSH23XUAvqdSDHNDq5T47mPoLmsz8yM
-fileName=modeloFisherFaces.xml
+# FICHER -> https://drive.google.com/file/d/1vQSYNSUOPY1QmdLpUxjmWhcM8jRxPKzd/view?usp=sharing
+fileId=1vQSYNSUOPY1QmdLpUxjmWhcM8jRxPKzd
+fileName=modelFisherFaces.xml
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${dirSaveFiles}/${fileName}
 
-# Eigen -> https://drive.google.com/file/d/1FEz7aIyIZ4TCgohMlw6kQ4kKN4e5hkpz/view?usp=sharing
-fileId=1FEz7aIyIZ4TCgohMlw6kQ4kKN4e5hkpz
-fileName=modeloEigenFaces.xml
+# Eigen -> https://drive.google.com/file/d/1rpaS5-iuun7BB4cQo0YUkME4IZ3TFiA0/view?usp=sharing
+fileId=1rpaS5-iuun7BB4cQo0YUkME4IZ3TFiA0
+fileName=modelEigenFaces.xml
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${dirSaveFiles}/${fileName}
 
-# LBPH -> https://drive.google.com/file/d/1HfotvvT3MJtXBoDGSBnMZKV2nkDAHas_/view?usp=sharing
-fileId=1HfotvvT3MJtXBoDGSBnMZKV2nkDAHas_
-fileName=modeloLBPH.xml
+# LBPH -> https://drive.google.com/file/d/1IwrY8G1Y8rOlCN-y90GKKKghVhpb4XlH/view?usp=sharing
+fileId=1IwrY8G1Y8rOlCN-y90GKKKghVhpb4XlH
+fileName=modelLBPH.xml
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${dirSaveFiles}/${fileName}
